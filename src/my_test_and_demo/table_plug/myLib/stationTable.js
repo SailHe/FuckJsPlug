2d6835cfc8145586ebcccf47b9280bbf342cb6d5
SailHe <396103442@qq.com>
2018-09-11 15:23:09 +0800

    Initial commit

---
+++ src/my_test_and_demo/table_plug/myLib/stationTable.js
@@ -0,0 +1,295 @@
+/**
+ * 奶站商品添加表格组件(需要引入特定的html)
+ * */
+
+//查重用
+var duplicateBuffer = {};
+
+var stationIdNameMap = new Map();
+
+function StationPriceDTO() {
+  this.stationPriceId = null;//Long id -->stationPriceId;
+  this.stationId = -1;//Integer stationId;
+  this.goodsSkuId = -1;//Long goodsSpecId --> goodsSkuId;
+  this.stationPriceFactoryPrice = 0;//BigDecimal factoryPrice --> stationPriceFactoryPrice;
+  this.stationPriceSalesPrice = 0;//BigDecimal salesPrice --> stationPriceSalesPrice;
+  /*this.delFlag = false; //Boolean stationPriceIsDel;*/
+}
+
+
+/**
+ * Descriptions: 为table弹框添加一行若<p>
+ * eg: $("#storehouse").addTableRow
+ * @author 何帆
+ * @date 2018/5/28 19:14
+ */
+$.fn.addStationTableRow = function (stationId, stationName) {
+  if (typeof stationId == "undefined") {
+    return;
+  }
+
+  var data = "<tr role='row'>" +
+    "<td><div style='text-align: center'><input style='padding:0 10px;' type='checkbox' name='stationCheckbox' value='" + stationId + "'></div></td>" +
+    "<td><div style='text-align: center'>" + stationName + "</div></td>" +
+    "</tr>"
+  $(this).append(data);
+}
+
+/**
+ * Descriptions: 为table弹框添加一行<p>
+ * eg: $("#storehouse").addTableRow
+ * @author 何帆
+ * @date 2018/5/28 19:14
+ */
+$.fn.addOrUpdateStationGoodsTableRow = function (name, stationGoodsDto) {
+  //查重
+  if (typeof duplicateBuffer[stationGoodsDto.stationId] == "undefined") {
+    duplicateBuffer[stationGoodsDto.stationId] = true;
+  } else if (duplicateBuffer[stationGoodsDto.stationId] == false) {
+    //将之前删除的重新'添加'
+    duplicateBuffer[stationGoodsDto.stationId] = true;
+    this.getTableColValue(0, function (cell, row) {
+      var $stationGoodsIdCell = $(row.get(0).cells[4]).find('input');
+      if (cell.val() == stationGoodsDto.stationId) {
+        //说起来没必要function的row参数了...
+        var row = cell.closest('tr');
+        row.find('.stationPriceFactoryPrice').get(0).textContent = stationGoodsDto.stationPriceFactoryPrice;
+        row.find('.stationPriceSalesPrice').get(0).textContent = stationGoodsDto.stationPriceSalesPrice;
+
+        //移除删除线
+        cell.tableRowDeleteLineRemove();
+        cell.removeAttr('class', 'pre-delete');
+
+        $stationGoodsIdCell.val(-parseInt($stationGoodsIdCell.val()));
+        //这里主要是遍历用 而不是为了获取列值
+        return false;
+      } else {
+        return false;
+      }
+    });
+    return;
+  } else {
+    //编辑已有的
+    this.getTableColValue(0, function ($cell) {
+      if ($cell.is(':checked')) {
+        var row = $cell.closest('tr');
+        row.find('.stationPriceFactoryPrice').get(0).textContent = stationGoodsDto.stationPriceFactoryPrice;
+        row.find('.stationPriceSalesPrice').get(0).textContent = stationGoodsDto.stationPriceSalesPrice;
+        //这里主要是遍历用 而不是为了获取列值
+        return false;
+      } else {
+        return false;
+      }
+    });
+    return;
+  }
+
+  var stationGoodsId = stationGoodsDto.stationPriceId == null ? 0 : stationGoodsDto.stationPriceId;
+
+  var data = "<tr role='row'>" +
+    "<td><div style='text-align: center'><input style='padding:0 10px;' type='checkbox' name='stationGoodsCheckbox' value='" + stationGoodsDto.stationId + "'></div></td>" +
+    "<td><div style='text-align: center'>" + name + "</div></td>" +
+    "<td><div style='text-align: center' class='stationPriceFactoryPrice'>" + stationGoodsDto.stationPriceFactoryPrice + "</div></td>" +
+    "<td><div style='text-align: center' class='stationPriceSalesPrice'>" + stationGoodsDto.stationPriceSalesPrice + "</div></td>" +
+    "<td style='display:none;'><div style='text-align: center;'><input class='stationGoodsId' value='" + stationGoodsId + "'></div></td>" +
+    "</tr>"
+  $(this).append(data);
+}
+
+/**
+ * Descriptions: 商品列表设置<p>
+ *
+ * @author 何帆
+ * @date 2018/5/28 19:14
+ */
+$.fn.setRelatedStationGoods = function () {
+  var stationIdListBuffer = this.getTableColValue(0);
+  var stationGoodsIdListBuffer = this.getTableColValue(4);
+  var factoryPricePriceListBuffer = this.getTableColValue(2);
+  var salesPriceListBuffer = this.getTableColValue(3);
+  $("input[name='stationGoodsIdList']").val(stationGoodsIdListBuffer);
+  $("input[name='stationIdList']").val(stationIdListBuffer);
+  $("input[name='factoryPriceList']").val(factoryPricePriceListBuffer);
+  $("input[name='salesPriceList']").val(salesPriceListBuffer);
+  return this;
+};
+
+$.fn.emptyStorehouse = function () {
+  //清空查重缓存
+  duplicateBuffer = {};
+  this.empty();
+  return this;
+}
+
+//绑定固有事件
+$("#stationInfoModal").on('hidden.bs.modal', function () {
+  //处理无法滚动bug @see: https://blog.csdn.net/forrest_chen/article/details/61423171
+  $(document.body).addClass("modal-open");
+});
+
+//请求所有奶站 并缓存
+function listStationRequest() {
+  $.ajax({
+    type: 'post',
+    dataType: 'json',
+    async: false,
+    url: 'myLib/station.json',
+    success: function (result) {
+      if (result.success) {
+        $("#stationStorehouse").empty();
+        for (var i = 0; i < result.data.length; i++) {
+          //缓存
+          stationIdNameMap.set(result.data[i].stationId, result.data[i].stationName);
+        }
+      } else {
+        console.log("获取奶站列表失败!");
+      }
+    }
+  });
+}
+
+//批量添加奶站商品价格
+$('#stationAddButton').on('click', function () {
+  $("#stationStorehouse").empty();
+  stationIdNameMap.forEach(function (value, key) {
+    //查重station modal
+    if (duplicateBuffer[key] == false || typeof duplicateBuffer[key] == "undefined") {
+      $("#stationStorehouse").addStationTableRow(key, value);
+    } else {
+      //do nothing
+    }
+  });
+  $('input[name=factoryPriceInput]').val('0');
+  $('input[name=salesPriceInput]').val('0');
+  $("#stationInfoModal").modal({
+    backdrop: 'static',
+    keyboard: false,
+    show: true
+  });
+});
+
+//批量修改奶站商品价格
+$('#stationEditButton').on('click', function () {
+  $("#stationStorehouse").empty();
+  var $stationGoodsStorehouse = $("#stationGoodsStorehouse");
+  var averOriginalPrice = 0;
+  var averPrice = 0;
+  var count = 0;
+  var stationIdList = $stationGoodsStorehouse.getTableColValue(0, function ($cell, $row) {
+    if ($cell.is(':checked')) {
+      ++count;
+      averOriginalPrice += parseFloat($row.get(0).cells[2].textContent);
+      averPrice += parseFloat($row.get(0).cells[3].textContent);
+      return true;
+    } else {
+      return false;
+    }
+  });
+  debugLog(stationIdList);
+  for (var i = 0; i < stationIdList.length; i++) {
+    $("#stationStorehouse").addStationTableRow(stationIdList[i], stationIdNameMap.get(parseInt(stationIdList[i])));
+  }
+
+  count == 0 ? count = 1 : 0;
+  $('input[name=factoryPriceInput]').val(averOriginalPrice / count);
+  $('input[name=salesPriceInput]').val(averPrice / count);
+
+  //选中所有
+  $("input[name='stationCheckbox']").attr("checked", true);
+  //$('input[name=stationCheckbox]').get(0).checked  = true;
+
+  $("#stationInfoModal").modal({
+    backdrop: 'static',
+    keyboard: false,
+    show: true
+  });
+});
+
+//批量删除奶站商品价格
+$('#stationGoodsDeleteButton').on('click', function () {
+  //这里主要是遍历用 而不是为了获取列值
+  $('#stationGoodsStorehouse').getTableColValue(0, function ($cell, row) {
+    if ($cell.is(':checked')) {
+      //查重缓存更新
+      var stationId = $cell.val();
+      var $stationGoodsIdCell = $(row.get(0).cells[4]).find('input');
+      if ($stationGoodsIdCell.val() == 0) {
+        //删除(实际删除 添加后又删除的 标志是没有stationGoodsId 即==0)
+        $cell.attr('class', 'deleter');
+        duplicateBuffer[stationId] = undefined;
+      } else {
+        //预删除(用于划删除线)
+        $cell.attr('class', 'pre-delete');
+        duplicateBuffer[stationId] = false;
+      }
+      $stationGoodsIdCell.val(-parseInt($stationGoodsIdCell.val()));
+      return false;
+    } else {
+      return false;
+    }
+  });
+  //批量删除
+  $('.deleter').deleteTableRow();//实际删除
+  $('.pre-delete').tableRowDeleteLine();//删除线
+});
+
+
+//绑定奶站表单的验证事件
+$('#stationInfoForm').bootstrapValidator({
+  feedbackIcons: {
+    valid: 'fa fa-ok',
+    invalid: 'fa fa-remove',
+    validating: 'fa fa-refresh'
+  },
+  fields: {
+    factoryPriceInput: {
+      validators: {
+        notEmpty: {
+          message: '非空！'
+        },
+      }
+    },
+    salesPriceInput: {
+      validators: {
+        notEmpty: {
+          message: '非空！'
+        }
+      }
+    },
+  }
+});
+
+//绑定奶站模态框 提交时的事件
+$('#stationSubmitButton').on('click', function () {
+  //提交验证: 非submit按钮点击后进行验证，如果是submit则无需此句直接验证
+  $("#stationInfoForm").bootstrapValidator('validate');
+  //获取验证结果，如果成功，执行下面代码
+  if ($("#stationInfoForm").data('bootstrapValidator').isValid()) {
+    //验证成功后的操作，如ajax
+    $('#stationInfoModal').trigger('station.modal.submit.success');
+    $('#stationInfoModal').modal('hide');
+  }
+});
+
+//全选按钮
+$('#allStationGoodsCheckBox').on('click', function () {
+  //this 是个Dom
+  if (this.textContent == '取消当前') {
+    $("input[name='stationGoodsCheckbox']").attr("checked", false);
+    this.textContent = '全选当前';
+  } else {
+    $("input[name='stationGoodsCheckbox']").attr("checked", true);
+    this.textContent = '取消当前';
+  }
+});
+
+$('#allStationCheckBox').on('click', function () {
+  //this 是个Dom
+  if (this.textContent == '取消当前') {
+    $("input[name='stationCheckbox']").attr("checked", false);
+    this.textContent = '全选当前';
+  } else {
+    $("input[name='stationCheckbox']").attr("checked", true);
+    this.textContent = '取消当前';
+  }
+});
